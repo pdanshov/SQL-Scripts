8e4e7fcd80583a88242ba8c1ccb29f01bebf67ad
pdanshov <pdanshv@gmail.com>
2014-11-06 05:36:19 -0500

    Thu Nov  6 05:35:32 EST 2014

--- jeff_edi.sql
+++ jeff_edi.sql
@@ -1,3 +1,4 @@
+BEGIN TRANSACTION
 delete from tblEdPoHeader
 delete from tblEdPoDetail
 delete from tblEdPoHistHeader
@@ -21,3 +22,30 @@ truncate table tbledpoerrorlog
 update tblEdPoHeader set CustPoNum='10370328t1' where CustPoNum='10370328'
 update tblEdPoDetail set CustPoNum='10370328t1' where CustPoNum='10370328'
 ROLLBACK
+
+select name + ', ' as [text()] 
+from sys.columns 
+where object_id = object_id('tblEdPoHeader') 
+for xml path('')
+
+SET IDENTITY_INSERT [001].dbo.tblEdPoHeader ON
+INSERT INTO [001].dbo.tblEdPoHeader (ID, ImportBatchId, PartnerID, SenderId, CustPoNum, PoDate, PurposeCode, TypeCode, StartShipDate, CancelShipDate, ContactName, ContactPhone, BuyerName, BuyerPhone, DepartmentID, MerchandiseTypeCode, ShipToCode, ShipToName, ShipToAddr1, ShipToAddr2, ShipToCity, ShipToRegion, ShipToCountry, ShipToPostalCode, BillToCode, BillToName, BillToAddr1, BillToAddr2, BillToCity, BillToRegion, BillToCountry, BillToPostalCode, TermsBasisDateCode, TermsDescr, TermsDiscDaysDue, TermsDiscPct, TermsDiscDueDate, TermsNetDays, TermsNetDueDate, TermsTypeCode, Duns, BackOrderAllowedYN, ShipAsapYN, ShipToStoreYN, UDF1, UDF2, UDF3, UDF4, UDF5)
+SELECT ID, ImportBatchId, PartnerID, SenderId, CustPoNum, PoDate, PurposeCode, TypeCode, StartShipDate, CancelShipDate, ContactName, ContactPhone, BuyerName, BuyerPhone, DepartmentID, MerchandiseTypeCode, ShipToCode, ShipToName, ShipToAddr1, ShipToAddr2, ShipToCity, ShipToRegion, ShipToCountry, ShipToPostalCode, BillToCode, BillToName, BillToAddr1, BillToAddr2, BillToCity, BillToRegion, BillToCountry, BillToPostalCode, TermsBasisDateCode, TermsDescr, TermsDiscDaysDue, TermsDiscPct, TermsDiscDueDate, TermsNetDays, TermsNetDueDate, TermsTypeCode, Duns, BackOrderAllowedYN, ShipAsapYN, ShipToStoreYN, UDF1, UDF2, UDF3, UDF4, UDF5 FROM [Test].dbo.tblEdPoHeader
+SET IDENTITY_INSERT [001].dbo.tblEdPoHeader OFF
+
+select name + ', ' as [text()] 
+from sys.columns 
+where object_id = object_id('tblEdPoDetail') 
+for xml path('')
+
+SET IDENTITY_INSERT [001].dbo.tblEdPoDetail ON
+INSERT INTO [001].dbo.tblEdPoDetail (ID, ImportBatchId, PartnerId, SenderId, CustPoNum, EntryNum, PoLineNum, StoreNum, QtyOrd, UnitsSell, UnitPrice, RetailPrice, QtyPack, UnitsPack, ItemUir, ItemBuyerCode, ItemVendorCode, ItemUpc, ItemID, ItemDecr, ColorDescr, SizeDescr, UDF1, UDF2, UDF3, UDF4, UDF5, IsNew, ErrorCode, UnitsSellPO, ManufTypeCode, SoUOMConversionFactor)
+SELECT ID, ImportBatchId, PartnerId, SenderId, CustPoNum, EntryNum, PoLineNum, StoreNum, QtyOrd, UnitsSell, UnitPrice, RetailPrice, QtyPack, UnitsPack, ItemUir, ItemBuyerCode, ItemVendorCode, ItemUpc, ItemID, ItemDecr, ColorDescr, SizeDescr, UDF1, UDF2, UDF3, UDF4, UDF5, IsNew, ErrorCode, UnitsSellPO, ManufTypeCode, SoUOMConversionFactor FROM [Test].dbo.tblEdPoDetail
+SET IDENTITY_INSERT [001].dbo.tblEdPoDetail OFF
+
+INSERT INTO [001].dbo.tblEdPoHistHeader
+SELECT * FROM [Test].dbo.tblEdPoHistHeader
+
+INSERT INTO [001].dbo.tblEdPoHistDetail
+SELECT * FROM [Test].dbo.tblEdPoHistDetail
+
