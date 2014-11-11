51f595aa3ffe4638cf54b8a4e44d5f52f1b565b1
pdanshov <pdanshv@gmail.com>
2014-11-11 05:36:13 -0500

    Tue Nov 11 05:35:32 EST 2014

--- AddBusinessRuleRecords.sql
+++ AddBusinessRuleRecords.sql
@@ -1,7 +1,25 @@
 
-/********* Peter Danshov - Add Business Rule Records to Table - 08.14.14 Thu 1533 EST pdanshv@gmail.com ***********/
+/************************************************************************
+ ********* Peter Danshov - Add Business Rule Records to Table ***********
+ ************************************************************************
+ *
+ *		pdanshv@gmail.com 11.10.14
+ *
+ *		sys smconfig 		   = groups and sub-group menu items
+ *		sys smconfigvalue 	   = new record with configref equal to smconfig
+ *		sys dbo.tblSmMenuDescr = main menu items
+ *		sys dbo.tblSmMenu	   = sub-menu items
+ *
+ *		All smconfig menu items must have captions:
+ *			Trav10.5: sys dbo.tbl.syscaption [ObjectID] = sys dbo.tblsmconfig [CaptionId]
+ *
+ *		Traverse Company Setup:
+ *			System Manager -> Company Setup -> Business Rules -> Application -> EDI
+ *
+ *************************************************************************/
 
-INSERT INTO [dbo].[tblSmConfig] (ConfigRef, RecType, AppId, ConfigId, CTConfigRef, OptConfigRef, DispSeq, Visible, RoleConfigYn, CaptionId, SrchID, DefaultValue, MaxWidth, RequiredYn, ValueListId, ReqAppId, ReadOnlyYn, MinValue, MaxValue)
+INSERT INTO [dbo].[tblSmConfig] (ConfigRef, RecType, AppId, ConfigId, CTConfigRef, OptConfigRef, DispSeq, Visible,
+ RoleConfigYn, CaptionId, SrchID, DefaultValue, MaxWidth, RequiredYn, ValueListId, ReqAppId, ReadOnlyYn, MinValue, MaxValue)
 VALUES
 /*EdiGrpFa*/ ( 10023, 2048, 'ED', 'EdiGrpFa', 356, 0, 10, 1, 0, 'Functional Acknowledgment, 997', NULL, NULL, 0, 0, NULL, NULL, 0, NULL, NULL ),
 	/*EdiFaFileName*/ ( 10017, 61, 'ED', 'EdiFaFileName', 10023, 0, 10, 1, 0, 'FA File Name', NULL, NULL, 0, 0, NULL, NULL, 0, NULL, NULL ),
@@ -18,11 +36,12 @@ VALUES
 /*EdiGrpGenDflt*/ ( 10024, 2048, 'ED', 'EdiGrpGenDflt', 356, 0, 20, 1, 0, 'Defaults - General', NULL, NULL, 0, 0, NULL, NULL, 0, NULL, NULL ),
 	/*EdiCommID*/ ( 10008, 0, 'ED', 'EdiCommID', 10024, 0, 100, 1, 0, 'Communication ID', NULL, NULL, 15, 0, NULL, NULL, 0, NULL, NULL ),
 	/*EdiCommQual*/ ( 10009, 0, 'ED', 'EdiCommQual', 10024, 0, 90, 1, 0, 'Communication Qualifier', NULL, NULL, 2, 0, NULL, NULL, 0, NULL, NULL ),
-	/*EDIDfltDuns*/ ( 10015, 0, 'ED', 'EDIDfltDuns', 10024, 0, 60, 1, 0, 'Default DUNS', NULL, NULL, 0, 0, NULL, NULL, 0, NULL, NULL ),
+	/*EDIDfltDuns*/ --( 10015, 0, 'ED', 'EDIDfltDuns', 10024, 0, 60, 1, 0, 'Default DUNS', NULL, NULL, 0, 0, NULL, NULL, 0, NULL, NULL ),
+	/*EDIDDuns*/ ( 10015, 0, 'ED', 'EDIDDuns', 10024, 0, 60, 1, 0, 'DUNS', NULL, NULL, 0, 0, NULL, NULL, 0, NULL, NULL ),
 	/*EdiGenArchivePath*/ ( 10019, 62, 'ED', 'EdiGenArchivePath', 10024, 0, 90, 1, 0, 'EDI Archive Directory', NULL, NULL, 0, 0, NULL, NULL, 0, NULL, NULL ),
 	/*EdiMasterPc*/ ( 10037, 0, 'ED', 'EdiMasterPc', 10024, 0, 110, 1, 0, 'EDI PC Name', NULL, NULL, 50, 0, NULL, NULL, 0, NULL, NULL ),
 	/*EdiLabelApplicationPath*/ --( 10036, 61, 'ED', 'EdiLabelApplicationPath', 10024, 0, 50, 1, 0, 'Label Application File Name', NULL, 'C:\Program Files\Seagull\BarTender 7.10\Professional\bartend.exe', 0, 0, NULL, NULL, 0, NULL, NULL ),
-	/*EdiLabelApplicationPath*/ ( 10036, 61, 'ED', 'EdiLabelApplicationPath', 10024, 0, 50, 1, 0, 'Label Application File Name', NULL, 0, 0, 0, NULL, NULL, 0, NULL, NULL ),
+	/*EdiLabelApplicationPath*/ ( 10036, 61, 'ED', 'EdiLabelApplicationPath', 10024, 0, 50, 1, 0, 'Label Application', NULL, 0, 0, 0, NULL, NULL, 0, NULL, NULL ),
 	/*EdiUCCPrefix*/ ( 10046, 0, 'ED', 'EdiUCCPrefix', 10024, 0, 30, 1, 0, 'UCC Company Prefix', NULL, 1, 8, 0, NULL, NULL, 0, NULL, NULL ),
 	/*EdiUCC128Seq*/ ( 10045, 275, 'ED', 'EdiUCC128Seq', 10024, 0, 60, 1, 0, 'UCC128 Sequence', NULL, 1, 0, 0, NULL, NULL, 0, NULL, NULL );
 
